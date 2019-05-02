019b9328699daaa2ab72222b1985f9bad0601b65
FSDonnelly <donnelly_frank@yahoo.com>
2019-05-01 22:14:46 -0700

    used mapStateToProps by using redux store to pass props to SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,10 +3,15 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>Song List</div>
         )
     }
 };
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
